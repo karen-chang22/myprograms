6670178f8f078d8197bfdfd4a13c4e5eba9049e7
karen.chang22 <chang.karen2008@gmail.com>
2025-09-15 10:58:04 -0400

    created a python file to be creepy.

---
+++ p1.py
@@ -0,0 +1,11 @@
+# My first program
+# Created on 09/15/2025
+# Purpose: to learn python
+# Editor: Karen Chang
+
+print("hello, world!")
+name = input("Hey there, can I have your name?: ")
+age = input("And your age?: ")
+print(f"Hi, {name}, you are {age} years old!")
+print("bye")
+
