a017224c50911acb5a93fcfdcee093c565122c4f
karen.chang22 <chang.karen2008@gmail.com>
2025-10-08 17:32:16 -0400

    karen commit

---
+++ strings1.py
@@ -0,0 +1,11 @@
+# String stuff - Oct 08
+# tacocat is a palindrome, so are doggod, 906609
+def is_palindrome(word):
+    # checks whether argument word is a palindrom
+    return word == word[::-1]
+
+text = input("enter a word: ")
+if is_palindrome(text):
+    print(f"{text} is a palindrome")
+else:
+    print(f"{text} is not a palindrome (boo)")
